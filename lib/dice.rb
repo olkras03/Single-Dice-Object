3ce27e2ca6691a9a55d8e4cf1471284c9ccf03da
Lena Krasnova <olkras03@gmail.com>
2019-10-22 10:28:23 +0100

    final version

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,4 +1,13 @@
 class Dice
-  def roll
+  def roll(number_of_dices = 1)
+    results = []
+    number_of_dices.times { results << dice_roll }
+    return results
   end
+end
+
+private
+
+def dice_roll
+  rand (1..6)
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,10 +1,29 @@
-require 'dice'
+require "dice"
 
-describe 'game' do
-  it 'should respond to the method dice' do
-    dice = Dice.new
+describe Dice do
+  let(:dice) { Dice.new }
+
+  it "is an instance of the class" do
+    expect(dice).to be_instance_of Dice
+  end
+
+  it "responds to the method dice" do
     expect(dice).to respond_to(:roll)
   end
 
+  it "allows user to roll a dice" do
+    expect(dice).to respond_to(:roll).with(1).argument
+  end
 
+  it "rolls dice and gives number between 1 and 6" do
+    expect(dice.roll).to all(be_between(1, 6))
+  end
+
+  it "gives the right number of results" do
+    expect(dice.roll(7).size).to eq 7
+  end
+
+  it "give all the results between 1 and 6" do
+    expect(dice.roll(10)).to all(be_between(1, 6))
+  end
 end
